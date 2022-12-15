716a048632357eeb1e583395c465a2b5ac94ab1b
kuzy53 <nikita129343@mail.ru>
2022-12-16 01:32:01 +0200

    dz2

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -17,14 +17,14 @@ export class Header {
         const recipe = document.createElement('div');
         const linkRecipe = document.createElement('a');
         linkRecipe.classList.add('links');
-        linkRecipe.href = '#';
+        linkRecipe.href = '';
         linkRecipe.innerHTML += 'Рецепты';
         recipe.append(linkRecipe);
 
         const journal = document.createElement('div');
         const linkJournal = document.createElement('a');
         linkJournal.classList.add('links');
-        linkJournal.href = '#';
+        linkJournal.href = '';
         linkJournal.innerHTML += 'Журнал';
         journal.append(linkJournal);
 
@@ -32,26 +32,26 @@ export class Header {
         const ideas = document.createElement('div');
         const linkIdeas = document.createElement('a');
         linkIdeas.classList.add('links');
-        linkIdeas.href = '#';
+        linkIdeas.href = '';
         linkIdeas.innerHTML += 'Идеи';
         ideas.append(linkIdeas);
 
-        const avtor = document.createElement('div');
-        const linkAvtor = document.createElement('a');
-        linkAvtor.classList.add('links');
-        linkAvtor.href = '#';
-        linkAvtor.innerHTML += 'Авторы';
-        avtor.append(linkAvtor);
+        const author = document.createElement('div');
+        const linkAuthor = document.createElement('a');
+        linkAuthor.classList.add('links');
+        linkAuthor.href = '';
+        linkAuthor.innerHTML += 'Авторы';
+        author.append(linkAuthor);
 
         const login = document.createElement('div');
         const linkLogin = document.createElement('a');
         linkLogin.classList.add('links');
-        linkLogin.href = '#';
+        linkLogin.href = '';
         linkLogin.innerHTML += 'Войти';
         login.append(linkLogin);
 
 
-        headerStyle.append(icons, recipe, journal, ideas, avtor, login);
+        headerStyle.append(icons, recipe, journal, ideas, author, login);
 
         header.appendChild(headerStyle);
 
